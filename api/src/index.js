ced711c1fc034c4809ef9eca478f8e85c222e6a9
Dimeliora <dimeliora710@gmail.com>
2022-01-26 19:48:06 +0900

    express-fileupload added

--- api/src/index.js
+++ api/src/index.js
@@ -1,18 +1,22 @@
 require('dotenv/config');
 const express = require('express');
+const filesUpload = require('express-fileupload');
 
 const dbConnect = require('./db/db-connection');
 const corsMW = require('./middlewares/cors-middleware');
 const authRouter = require('./routes/auth.routes');
+const fileRouter = require('./routes/file.routes');
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 
 app.use(express.json());
+app.use(filesUpload());
 app.use(corsMW);
 
 app.use('/api/auth', authRouter);
+app.use('/api/file', fileRouter);
 
 const startup = async () => {
     try {

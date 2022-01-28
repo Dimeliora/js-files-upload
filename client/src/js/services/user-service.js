7b1132abdbbe70ea3cf3ff3b62f8017122352c53
Dimeliora <dimeliora710@gmail.com>
2022-01-28 16:37:08 +0900

    user-service for user data fetching

---
+++ client/src/js/services/user-service.js
@@ -0,0 +1,23 @@
+import axios from 'axios';
+
+import { BASE_URL } from '../config/base-url';
+
+const userService = axios.create({
+    baseURL: `${BASE_URL}/user`,
+});
+
+export const getUserData = async () => {
+    const accessToken = localStorage.getItem('access-token');
+
+    try {
+        const { data } = await userService.get('/', {
+            headers: {
+                Authorization: `Bearer ${accessToken}`,
+            },
+        });
+
+        return data;
+    } catch (error) {
+        throw new Error(error.response.data.message);
+    }
+};
